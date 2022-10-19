a426901033dfd298f9fc2cdda36fef98e5668e6e
Zivanai <zivanaiarnolddoma@gmail.com>
2022-10-19 19:53:56 +0300

    Add diplay book button for info entry

--- app.js
+++ app.js
@@ -1,17 +1,68 @@
 const libraryBooks = [];
 
-function Book(author, title, pages, status) {
-    this.author = (author)? author : "Unknown";
-    this.title = (title)? title : "Unknown";
-    this.pages = (pages)? pages : null;
-    this.status = (status)? status : "Unknown"
+function Book(bookTitle,
+    bookAuthor,
+    bookPageCount,
+    bookStatus,
+    bookAdditionTimestamp,
+    bookOwner,) {
+        this.bookTitle = (bookTitle)? bookTitle : "Unknown";
+        this.bookAuthor = (bookAuthor)? bookAuthor : "Unknown";
+        this.bookPageCount = (bookPageCount)? bookPageCount : null;
+        this.bookStatus = (bookStatus)? bookStatus : "Unread";
+        this.bookAdditionTimestamp = (bookAdditionTimestamp)? bookAdditionTimestamp : null;
+        this.bookOwner = (bookOwner)? bookOwner : "Unknown";
+}
+
+/*
+returns an array of new book property values from an HTML form
+1. {bookTitle :text}
+2. {bookAuthor :text}
+3. {bookPageCount :number}
+4. {bookStatus :text}
+5. {bookAdditionTimestamp :datetime}
+6. {bookOwner :text}
+*/
+function getNewBookData() {
+    const data = [];
+    const bookProperties = document.querySelectorAll(".bookProperty");
+    for (let prop of bookProperties) {
+        data.push(prop.value);
+    }
+    return data
 }
 
 /*
 returns Success message with added book title
 */
 function addNewBook() {
-    const newBook = new Book (author, title, pages, status)
+    const data = getNewBookData()
+    const newBook = new Book (...data)
     libraryBooks.push(newBook)
     return `Added new book with title: ${newBook.author}`
+}
+
+/*
+@params :
+*/
+function addBookToShelf(book, index) {
+    const bookShelf = document.querySelector("#bookShelf");
+    const tableRow = document.createElement("tr");
+    tableRow.setAttribute(`data-${index}`, index);
+    for (let i = 0; i < 3; i++) {
+        const td = document.createElement("td");
+        td.textContent = book[i];
+        tableRow.appendChild(td);
+    }
+    bookShelf.appendChild(tableRow);
+}
+
+// events
+const addBookButtons = document.querySelectorAll(".displayButton");
+for (let button of addBookButtons) {
+    button.addEventListener("click", displayForm)
+}
+function displayForm() {
+    const popUpForm = document.querySelector(".bookDetails.popup-card");
+    popUpForm.classList.toggle("hidden");
 }
