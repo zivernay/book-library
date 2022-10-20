b601180a40277579afef5ad0bfb0367d677edef8
Zivanai <zivanaiarnolddoma@gmail.com>
2022-10-20 19:10:36 +0300

    Add documentation comments to js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const addBookButtons = document.querySelectorAll(".displayButton");
 const libraryBooks = [];
+const saveBookButton = document.querySelector(".saveBook");
 
+//book constructor
 function Book(bookTitle,
     bookAuthor,
     bookPageCount,
@@ -14,27 +17,29 @@ function Book(bookTitle,
         this.bookOwner = (bookOwner)? bookOwner : "Unknown";
 }
 
-
-// events
-const addBookButtons = document.querySelectorAll(".displayButton");
-for (let button of addBookButtons) {
-    button.addEventListener("click", displayForm)
-}
+/*
+Show or hide the form for book properties
+@paras:
+    none
+returns:
+    none
+*/
 function displayForm() {
     const popUpForm = document.querySelector(".bookDetails.popup-card");
     popUpForm.classList.toggle("hidden");
 }
 
-const saveBookButton = document.querySelector(".saveBook");
-saveBookButton.addEventListener("click", createNewBook);
 /*
-returns an array of new book property values from an HTML form
-1. {bookTitle :text}
-2. {bookAuthor :text}
-3. {bookPageCount :number}
-4. {bookStatus :text}
-5. {bookAdditionTimestamp :datetime}
-6. {bookOwner :text}
+Read the form data and return a list of book properties:
+@params: 
+    none
+returns:
+    0. {bookTitle :text}
+    1. {bookAuthor :text}
+    2. {bookPageCount :number}
+    3. {bookStatus :text}
+    4. {bookAdditionTimestamp :datetime}
+    5. {bookOwner :text}
 */
 function getNewBookData() {
     const data = [];
@@ -45,26 +50,25 @@ function getNewBookData() {
     return data
 }
 
+/*
+Clear all form data
+@params:
+    none
+returns:
+    none
+*/
 function resetForm() {
     const dataForm = document.querySelector(".dataForm");
     dataForm.reset();
 }
-/*
-returns Success message with added book title
-*/
-function createNewBook() {
-    const data = getNewBookData()
-    const newBook = new Book (...data)
-    libraryBooks.push(newBook)
-    const index = libraryBooks.indexOf(newBook);
-    addBookToShelf(data, index)
-    alert `${newBook.author} successfully added`
-    displayForm();
-    resetForm();
-}
 
 /*
+Display book properties to into the bookShelf table
 @params :
+    bookProperties: array -> contain book properties as string
+    index: number -> position of the book in the {bookLibrary} array being used as id
+return:
+    none
 */
 function addBookToShelf(bookProperties, index) {
     const bookShelf = document.querySelector("#bookShelf");
@@ -77,3 +81,29 @@ function addBookToShelf(bookProperties, index) {
     }
     bookShelf.appendChild(tableRow);
 }
+
+/*
+Use the fetched data to create a book object and add it to books
+@params:
+    none
+return:
+    string ->Success message with added book title
+*/
+function createNewBook() {
+    const data = getNewBookData()
+    const newBook = new Book (...data)
+    libraryBooks.push(newBook)
+    const index = libraryBooks.indexOf(newBook);
+    addBookToShelf(data, index);
+    alert(`${newBook.bookTitle} by ${newBook.bookAuthor} successfully added`);
+    displayForm();
+    resetForm();
+}
+
+
+// events
+for (let button of addBookButtons) {
+    //Toggle the popup form to display and disappear
+    button.addEventListener("click", displayForm)
+}
+saveBookButton.addEventListener("click", createNewBook);
